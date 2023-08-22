9c1c362a06e1486fbf1bad9e65d4ac0b1fa13b95
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-22 11:26:32 +0200

    Set up route, action and view for /spaceships

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  resources :spaceships, only: %i[index]
+
   devise_for :users
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :spaceship, only: %i[index]
 end
