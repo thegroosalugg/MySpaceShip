0402a26de6c4fc1564fba68880b3e6b5e900f234
Malte Asmuth <malte.olaf.asmuth@web.de>
2023-08-23 12:07:37 +0200

    Fixed creation method and implemented user and spaceship ids to the booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -4,14 +4,25 @@ class BookingsController < ApplicationController
     @spaceship = Spaceship.find(params["spaceship_id"])
   end
 
-  # def create
-  #   @booking = Booking.new(bookings_params)
-  #   if @booking.save
-  #     redirect_to spaceships_bookings_path(@booking)
-  #   else
-  #     render :new, status: :unprocessable_entity
-  #   end
-  # end
+  def create
+    @booking = Booking.new(bookings_params)
+    @spaceship = Spaceship.find(params["spaceship_id"])
+
+    user = current_user
+    spaceship_id = @spaceship.id.to_i
+
+    @booking.user_id = current_user.id
+    @booking.spaceship_id = spaceship_id
+
+    if @booking.save
+      redirect_to spaceships_bookings_path(@booking)
+    else
+      render :new, status: :unprocessable_entity
+    end
+
+    
+
+  end
 
   private
 
@@ -20,6 +31,6 @@ class BookingsController < ApplicationController
   end
 
   def bookings_params
-    params.require(:booking).permit(:name, :photo)
+    params.require(:booking).permit(:name, :start_date, :end_date, :spaceship_id)
   end
 end
