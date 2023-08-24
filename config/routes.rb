5f7d8f36bf0700c84a9f92ce857f9535199b1731
Victor Loginov <tkoe88@zoho.com>
2023-08-24 12:27:46 +0200

    Created basic bookings dashboard with full functionality but a lack of badman style

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'dashboard', to: 'pages#dashboard'
 
   root to: "pages#home"
   resources :spaceships, only: %i[index show new create edit update] do
